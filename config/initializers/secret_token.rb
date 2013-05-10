37ebe0e388964703a3806f184011e4d29ce670d2
Wlodek Bzyl <matwb@univ.gda.pl>
2013-05-10 11:53:12 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FortuneResponders4X::Application.config.secret_key_base = '41649de36fbfbe610e0f9f699033731887b17a64c205312f2db9c7a60c60b6b99c6ab8d1fc0b7fb77487be41e696863af62f8057744d101ffb683f271ae7bf39'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FortuneResponders4X::Application.config.session_store :cookie_store, key: '_fortune-responders-4_x_session'
