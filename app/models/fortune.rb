fb5c7cee04dc1b009741a207502783faeb063ffb
Wlodek Bzyl <matwb@univ.gda.pl>
2013-05-10 14:02:56 +0200

    add validation to fortunes

--- app/models/fortune.rb
+++ app/models/fortune.rb
@@ -1,2 +1,7 @@
 class Fortune < ActiveRecord::Base
+  validates :quotation, presence: true
+  validates :quotation, length: { maximum: 256 }
+  validates :quotation, uniqueness: { case_sensitive: false }
+
+  validates :source, length: { in: 3..64 }, allow_blank: true
 end
