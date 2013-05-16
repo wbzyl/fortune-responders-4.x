e7f559a4e8c89b8537b99e8149e5b1fa4aabc3fa
Wlodek Bzyl <matwb@univ.gda.pl>
2013-05-16 13:39:45 +0200

    dodano comments w widoku show dla fortunek

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130510104214) do
+ActiveRecord::Schema.define(version: 20130516110147) do
+
+  create_table "comments", force: true do |t|
+    t.integer  "fortune_id"
+    t.string   "body"
+    t.string   "author"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["fortune_id"], name: "index_comments_on_fortune_id"
 
   create_table "fortunes", force: true do |t|
     t.text     "quotation"
